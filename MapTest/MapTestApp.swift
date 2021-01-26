1c17d5915851e22f813d2d4b86356a56736ca4b3
derekn4 <derekn4@uci.edu>
2021-01-26 10:31:26 -0800

    Initial Commit

---
+++ MapTest/MapTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTestApp.swift
+//  MapTest
+//
+//  Created by Derek Nguyen on 1/25/21.
+//
+
+import SwiftUI
+
+@main
+struct MapTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
